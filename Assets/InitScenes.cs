1ccf8bdaaca4e5c70776576cfbb52c0d699c8cb2
MSPK-git <115220663+MSPK-git@users.noreply.github.com>
2022-11-22 17:27:48 +0100

    add hands

---
+++ Assets/InitScenes.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Verkauf", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Werkstatt", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Lager", LoadSceneMode.Additive);
+}
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
